7d549a3b05baf8690aafef9b2296fa25550bd2ed
rkalahasty (forge) <88392030+rkalahasty@users.noreply.github.com>
2024-11-21 00:42:36 -0800

    fix: Replace unsupported aws_s3_buckets data source with AWS CLI command

--- delete_all_s3_buckets.tf
+++ delete_all_s3_buckets.tf
@@ -2,14 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_s3_buckets" "all" {}
-
 resource "null_resource" "delete_buckets" {
-  count = length(data.aws_s3_buckets.all.names)
-
   provisioner "local-exec" {
     command = <<EOT
-      aws s3 rb s3://${data.aws_s3_buckets.all.names[count.index]} --force
+      for bucket in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do
+        aws s3 rb s3://$bucket --force
+      done
     EOT
   }
 }
