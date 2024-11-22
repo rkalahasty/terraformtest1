6e9d4b289496f302cd55d616b6331632263f2f71
rkalahasty (forge) <88392030+rkalahasty@users.noreply.github.com>
2024-11-22 13:36:46 -0800

    feat: Add public access block configuration for S3 bucket

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ terraform {
   required_version = ">= 0.11.0"
 }
 
+resource "aws_s3_bucket_public_access_block" "LindsaysBucket" {
+  bucket = aws_s3_bucket.LindsaysBucket.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket" "LindsaysBucket" {
   bucket = "lindsays-bucket"
   acl    = "private"
